9941e0d0b6a3d031a3a5c2130697750f1934498b
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-04-27 21:31:43 +0800

    1

--- examples/transformer-xl/scripts/run_enwik8_basic.sh
+++ examples/transformer-xl/scripts/run_enwik8_basic.sh
@@ -20,7 +20,7 @@ if [[ $1 == 'train' ]]; then
         --tgt_len 512 \
         --mem_len 512 \
         --eval_tgt_len 128 \
-        --batch_size 22
+        --batch_size 22 \
         ${@:2}
 elif [[ $1 == 'eval' ]]; then
     echo 'Run evaluation...'
