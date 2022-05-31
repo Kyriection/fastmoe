d70b3ad39dc23d5b02ad24cd54fbef35feba528f
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-05-31 22:11:49 +0800

    Update run_enwik8_base12_randassignmoe.sh

--- examples/transformer-xl/scripts/run_enwik8_base12_randassignmoe.sh
+++ examples/transformer-xl/scripts/run_enwik8_base12_randassignmoe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ $1 == 'train' ]]; then
     echo 'Run training...'
-    python train.py \
+    python -u train.py \
         --cuda \
         --data ../data/enwik8/ \
         --dataset enwik8 \
@@ -27,7 +27,7 @@ if [[ $1 == 'train' ]]; then
         ${@:2}
 elif [[ $1 == 'eval' ]]; then
     echo 'Run evaluation...'
-    python eval.py \
+    python -u eval.py \
         --cuda \
         --data ../data/enwik8/ \
         --dataset enwik8 \
