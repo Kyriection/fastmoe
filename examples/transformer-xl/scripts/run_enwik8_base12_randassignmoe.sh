574da9628afa17da02013282c4b4d75d4ba4e9af
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-05-31 21:57:23 +0800

    1

--- examples/transformer-xl/scripts/run_enwik8_base12_randassignmoe.sh
+++ examples/transformer-xl/scripts/run_enwik8_base12_randassignmoe.sh
@@ -19,8 +19,9 @@ if [[ $1 == 'train' ]]; then
         --tgt_len 512 \
         --mem_len 512 \
         --eval_tgt_len 128 \
-        --batch_size 24 \
+        --batch_size 22 \
         --multi_gpu \
+        --gpu0_bsz 4 \
         --moe --moe-num-expert 64 --moe-top-k 2 \
         --gate_name CustomRandomGate \
         ${@:2}
