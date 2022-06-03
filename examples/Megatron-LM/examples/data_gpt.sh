2f3f93132e2e9e62cbc6ea82b19622b421e37943
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-06-03 17:10:58 +0800

    Create data_gpt.sh

---
+++ examples/Megatron-LM/examples/data_gpt.sh
@@ -0,0 +1,8 @@
+python tools/preprocess_data.py \
+    --input my-corpus.json \
+    --output-prefix my-gpt2 \
+    --vocab gpt2-vocab.json \
+    --dataset-impl mmap \
+    --tokenizer-type GPT2BPETokenizer \
+    --merge-file gpt2-merges.txt \
+    --append-eod
