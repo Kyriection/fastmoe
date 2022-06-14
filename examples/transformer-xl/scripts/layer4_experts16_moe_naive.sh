c8edcb69af0c6aa6ed31b04e83ea373a84bd68f4
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-06-14 19:18:16 +0800

    Update layer4_experts16_moe_naive.sh

--- examples/transformer-xl/scripts/layer4_experts16_moe_naive.sh
+++ examples/transformer-xl/scripts/layer4_experts16_moe_naive.sh
@@ -20,9 +20,11 @@ if [[ $1 == 'train' ]]; then
         --mem_len 512 \
         --eval_tgt_len 128 \
         --batch_size 22 \
+        --multi_gpu \
+        --gpu0_bsz 4 \
         --moe --moe-num-expert 16 --moe-top-k 2 \
         --gate_name CustomNaiveGate \
-        --eval-interval 100 \
+
         ${@:2}
 elif [[ $1 == 'eval' ]]; then
     echo 'Run evaluation...'
