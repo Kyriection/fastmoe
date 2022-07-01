e6661365a515eec928687adb62e3c1fcc8eb4e2c
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-07-01 23:12:55 +0800

    1

--- examples/transformer-xl/custom_layers.py
+++ examples/transformer-xl/custom_layers.py
@@ -6,12 +6,12 @@ import os
 import torch
 import torch.nn as nn
 
-from .functions import prepare_forward, ensure_comm
-from .functions import MOEScatter, MOEGather
-from .functions import AllGather, Slice
-from .gates import NaiveGate
+from functions import prepare_forward, ensure_comm
+from functions import MOEScatter, MOEGather
+from functions import AllGather, Slice
+from gates import NaiveGate
 
-from .fastermoe.config import switch_from_env
+from fastermoe.config import switch_from_env
 
 
 def mark_module_parallel_comm(module, comm):
