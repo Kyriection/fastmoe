979b8ff0e7492f8fc3e7306721c0fcd2ab61dead
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-07-06 01:01:57 +0800

    Update flops.sh

--- examples/transformer-xl/scripts/flops.sh
+++ examples/transformer-xl/scripts/flops.sh
@@ -10,7 +10,7 @@ if [[ $1 == 'train' ]]; then
         --d_model 256 \
         --n_head 8 \
         --d_head 64 \
-        --d_inner 1024 \
+        --d_inner 32768 \
         --dropout 0.1 \
         --dropatt 0.0 \
         --optim adam \
