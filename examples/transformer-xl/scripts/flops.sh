b86a1cebe05bffa10be46bf601c54201a24e98fb
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-07-06 01:05:34 +0800

    Update flops.sh

--- examples/transformer-xl/scripts/flops.sh
+++ examples/transformer-xl/scripts/flops.sh
@@ -21,6 +21,8 @@ if [[ $1 == 'train' ]]; then
         --mem_len 512 \
         --eval_tgt_len 128 \
         --batch_size 22 \
+        --moe --moe-num-expert 64 --moe-top-k 2 \
+        --gate_name CustomDropGate \
         ${@:2}
 elif [[ $1 == 'eval' ]]; then
     echo 'Run evaluation...'
