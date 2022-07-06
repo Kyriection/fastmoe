fca30500a4c068ee0ec3bc468d490598e867c97b
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-07-06 21:30:22 +0800

    Update flops.sh

--- examples/transformer-xl/scripts/flops.sh
+++ examples/transformer-xl/scripts/flops.sh
@@ -10,7 +10,7 @@ if [[ $1 == 'train' ]]; then
         --d_model 256 \
         --n_head 8 \
         --d_head 64 \
-        --d_inner 512 \
+        --d_inner 4096 \
         --dropout 0.1 \
         --dropatt 0.0 \
         --optim adam \
@@ -21,9 +21,7 @@ if [[ $1 == 'train' ]]; then
         --mem_len 512 \
         --eval_tgt_len 128 \
         --batch_size 22 \
-        --moe --moe-num-expert 64 --moe-top-k $2 \
-        --gate_name CustomRandomGate --gradual_moe \
-        ${@:3}
+        ${@:2}
 elif [[ $1 == 'eval' ]]; then
     echo 'Run evaluation...'
     python eval.py \
