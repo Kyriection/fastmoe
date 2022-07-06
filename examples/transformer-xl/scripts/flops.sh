684faaf57cce87d7aae23f3a5a8c23c7bac06f49
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-07-06 09:17:06 +0800

    1

--- examples/transformer-xl/scripts/flops.sh
+++ examples/transformer-xl/scripts/flops.sh
@@ -6,24 +6,24 @@ if [[ $1 == 'train' ]]; then
         --cuda \
         --data ../data/enwik8/ \
         --dataset enwik8 \
-        --n_layer 12 \
-        --d_model 512 \
+        --n_layer 4 \
+        --d_model 256 \
         --n_head 8 \
         --d_head 64 \
-        --d_inner 2048 \
+        --d_inner 512 \
         --dropout 0.1 \
         --dropatt 0.0 \
         --optim adam \
         --lr 0.00025 \
         --warmup_step 0 \
-        --max_step 5 \
+        --max_step 400000 \
         --tgt_len 512 \
         --mem_len 512 \
         --eval_tgt_len 128 \
         --batch_size 22 \
-        --moe --moe-num-expert 16 --moe-top-k 2 \
-        --gate_name CustomDropGate \
-        ${@:2}
+        --moe --moe-num-expert 16 --moe-top-k $2 \
+        --gate_name CustomRandomGate --gradual_moe \
+        ${@:3}
 elif [[ $1 == 'eval' ]]; then
     echo 'Run evaluation...'
     python eval.py \
