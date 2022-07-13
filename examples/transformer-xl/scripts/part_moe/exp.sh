e1981ea308e01b31f0cc602260d00153cf320018
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-07-13 13:15:02 +0800

    Update exp.sh

--- examples/transformer-xl/scripts/part_moe/exp.sh
+++ examples/transformer-xl/scripts/part_moe/exp.sh
@@ -14,8 +14,8 @@
 # CUDA_VISIBLE_DEVICES=1 nohup bash scripts/part_moe/layer4_eval.sh CustomNaiveGate ${key} 0,1,2,3 > log_EVAL_${key}.out 2>&1 &
 # key=layer4_experts16_moe_naive_every2-enwik8 
 # CUDA_VISIBLE_DEVICES=2 nohup bash scripts/part_moe/layer4_eval.sh CustomNaiveGate ${key} 1,3 > log_EVAL_${key}.out 2>&1 &
-# key=layer4_experts16_moe_naive-enwik8
-# CUDA_VISIBLE_DEVICES=3 nohup bash scripts/part_moe/layer4_eval.sh CustomNaiveGate ${key} 0,1,2,3 > log_EVAL_${key}.out 2>&1 &
+key=layer4_experts16_moe_naive-enwik8
+CUDA_VISIBLE_DEVICES=3 nohup bash scripts/part_moe/layer4_eval.sh CustomNaiveGate ${key} 0,1,2,3 > log_EVAL_${key}.out 2>&1 &
 # key=layer4_experts16_moe_gradual_naive_reverse-enwik8
 # CUDA_VISIBLE_DEVICES=0 nohup bash scripts/part_moe/layer4_eval.sh CustomNaiveGate ${key} 0,1,2,3 > log_EVAL_${key}.out 2>&1 &
 # key=layer4_experts16_moe_gradual_naive-enwik8
@@ -35,22 +35,22 @@
 
 
 
-key=layer4_experts16_moe_random_late-enwik8
-CUDA_VISIBLE_DEVICES=0 nohup bash scripts/part_moe/layer4_eval.sh CustomRandomGate ${key} 3 > log_EVAL_${key}.out 2>&1 &
-key=layer4_experts16_moe_random_early-enwik8
-CUDA_VISIBLE_DEVICES=1 nohup bash scripts/part_moe/layer4_eval.sh CustomRandomGate ${key} 0 > log_EVAL_${key}.out 2>&1 &
-key=layer4_experts16_moe_naive_late-enwik8
-CUDA_VISIBLE_DEVICES=2 nohup bash scripts/part_moe/layer4_eval.sh CustomNaiveGate ${key} 3 > log_EVAL_${key}.out 2>&1 &
-key=layer4_experts16_moe_naive_freeze_middle1-enwik8
-CUDA_VISIBLE_DEVICES=3 nohup bash scripts/part_moe/layer4_eval.sh CustomNaiveGate ${key} 1 > log_EVAL_${key}.out 2>&1 &
-key=layer4_experts16_moe_naive_freeze_late-enwik8
-CUDA_VISIBLE_DEVICES=0 nohup bash scripts/part_moe/layer4_eval.sh CustomNaiveGate ${key} 3 > log_EVAL_${key}.out 2>&1 &
-key=layer4_experts16_moe_naive_freeze_every2-enwik8
-CUDA_VISIBLE_DEVICES=1 nohup bash scripts/part_moe/layer4_eval.sh CustomNaiveGate ${key} 1,3 > log_EVAL_${key}.out 2>&1 &
-key=layer4_experts16_moe_naive_freeze_early-enwik8
-CUDA_VISIBLE_DEVICES=2 nohup bash scripts/part_moe/layer4_eval.sh CustomNaiveGate ${key} 0 > log_EVAL_${key}.out 2>&1 &
-key=layer4_experts16_moe_naive_early-enwik8
-CUDA_VISIBLE_DEVICES=3 nohup bash scripts/part_moe/layer4_eval.sh CustomNaiveGate ${key} 0 > log_EVAL_${key}.out 2>&1 &
+# key=layer4_experts16_moe_random_late-enwik8
+# CUDA_VISIBLE_DEVICES=0 nohup bash scripts/part_moe/layer4_eval.sh CustomRandomGate ${key} 3 > log_EVAL_${key}.out 2>&1 &
+# key=layer4_experts16_moe_random_early-enwik8
+# CUDA_VISIBLE_DEVICES=1 nohup bash scripts/part_moe/layer4_eval.sh CustomRandomGate ${key} 0 > log_EVAL_${key}.out 2>&1 &
+# key=layer4_experts16_moe_naive_late-enwik8
+# CUDA_VISIBLE_DEVICES=2 nohup bash scripts/part_moe/layer4_eval.sh CustomNaiveGate ${key} 3 > log_EVAL_${key}.out 2>&1 &
+# key=layer4_experts16_moe_naive_freeze_middle1-enwik8
+# CUDA_VISIBLE_DEVICES=3 nohup bash scripts/part_moe/layer4_eval.sh CustomNaiveGate ${key} 1 > log_EVAL_${key}.out 2>&1 &
+# key=layer4_experts16_moe_naive_freeze_late-enwik8
+# CUDA_VISIBLE_DEVICES=0 nohup bash scripts/part_moe/layer4_eval.sh CustomNaiveGate ${key} 3 > log_EVAL_${key}.out 2>&1 &
+# key=layer4_experts16_moe_naive_freeze_every2-enwik8
+# CUDA_VISIBLE_DEVICES=1 nohup bash scripts/part_moe/layer4_eval.sh CustomNaiveGate ${key} 1,3 > log_EVAL_${key}.out 2>&1 &
+# key=layer4_experts16_moe_naive_freeze_early-enwik8
+# CUDA_VISIBLE_DEVICES=2 nohup bash scripts/part_moe/layer4_eval.sh CustomNaiveGate ${key} 0 > log_EVAL_${key}.out 2>&1 &
+# key=layer4_experts16_moe_naive_early-enwik8
+# CUDA_VISIBLE_DEVICES=3 nohup bash scripts/part_moe/layer4_eval.sh CustomNaiveGate ${key} 0 > log_EVAL_${key}.out 2>&1 &
 
 
 
