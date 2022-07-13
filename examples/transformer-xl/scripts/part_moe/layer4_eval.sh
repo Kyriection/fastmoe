43cca7a0210166295478d158e5edd3fb3f25beed
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-07-13 11:22:15 +0800

    1

---
+++ examples/transformer-xl/scripts/part_moe/layer4_eval.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo 'Run Evaluation...'
+python -u train.py \
+    --cuda \
+    --data ../data/enwik8/ \
+    --dataset enwik8 \
+    --n_layer 4 \
+    --d_model 256 \
+    --n_head 8 \
+    --d_head 64 \
+    --d_inner 512 \
+    --dropout 0.1 \
+    --dropatt 0.0 \
+    --optim adam \
+    --lr 0.00025 \
+    --warmup_step 0 \
+    --max_step 400000 \
+    --tgt_len 512 \
+    --mem_len 512 \
+    --eval_tgt_len 128 \
+    --batch_size 22 \
+    --moe --moe-num-expert 16 --moe-top-k 2 \
+    --gate_name $1 \
+    --work_dir $2
