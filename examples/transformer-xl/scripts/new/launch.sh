26a7b4d3f1e05ff91b088b9928ebf9048ea7247a
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-07-14 21:05:56 +0800

    Update launch.sh

--- examples/transformer-xl/scripts/new/launch.sh
+++ examples/transformer-xl/scripts/new/launch.sh
@@ -1,8 +1,53 @@
+# #####################  UT VITA1 #################
+# DATE=0714
+# GATE_NAME=CustomNaiveGate
+# MoE_INDEX=0,1,2,3
+# Dynamic_mode=linear_increase
+# Dynamic_steps=400000
+# SAVE_DIR=Layer4_Experts16_${GATE_NAME}_ALL_${Dynamic_mode}_${Dynamic_steps}
+# CUDA_VISIBLE_DEVICES=0 nohup bash scripts/new/layer4_experts16_moe_freeze_router.sh \
+#     ${GATE_NAME} ${MoE_INDEX} ${Dynamic_mode} ${Dynamic_steps} ${SAVE_DIR} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
+
+# DATE=0714
+# GATE_NAME=CustomNaiveGate
+# MoE_INDEX=0,1,2,3
+# Dynamic_mode=linear_decrease
+# Dynamic_steps=400000
+# SAVE_DIR=Layer4_Experts16_${GATE_NAME}_ALL_${Dynamic_mode}_${Dynamic_steps}
+# CUDA_VISIBLE_DEVICES=1 nohup bash scripts/new/layer4_experts16_moe_freeze_router.sh \
+#     ${GATE_NAME} ${MoE_INDEX} ${Dynamic_mode} ${Dynamic_steps} ${SAVE_DIR} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
+
+# DATE=0714
+# GATE_NAME=CustomNaiveGate
+# MoE_INDEX=0,1,2,3
+# Dynamic_mode=cosine_decrease
+# Dynamic_steps=400000
+# SAVE_DIR=Layer4_Experts16_${GATE_NAME}_ALL_${Dynamic_mode}_${Dynamic_steps}
+# CUDA_VISIBLE_DEVICES=2 nohup bash scripts/new/layer4_experts16_moe_freeze_router.sh \
+#     ${GATE_NAME} ${MoE_INDEX} ${Dynamic_mode} ${Dynamic_steps} ${SAVE_DIR} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
+
+# DATE=0714
+# GATE_NAME=CustomNaiveGate
+# MoE_INDEX=0,1,2,3
+# Dynamic_mode=cosine_increase
+# Dynamic_steps=400000
+# SAVE_DIR=Layer4_Experts16_${GATE_NAME}_ALL_${Dynamic_mode}_${Dynamic_steps}
+# CUDA_VISIBLE_DEVICES=3 nohup bash scripts/new/layer4_experts16_moe_freeze_router.sh \
+#     ${GATE_NAME} ${MoE_INDEX} ${Dynamic_mode} ${Dynamic_steps} ${SAVE_DIR} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
+
+
+
+
+
+
+
+
+#####################  UT VITA3 #################
 DATE=0714
 GATE_NAME=CustomNaiveGate
 MoE_INDEX=0,1,2,3
 Dynamic_mode=linear_increase
-Dynamic_steps=400000
+Dynamic_steps=200000
 SAVE_DIR=Layer4_Experts16_${GATE_NAME}_ALL_${Dynamic_mode}_${Dynamic_steps}
 CUDA_VISIBLE_DEVICES=0 nohup bash scripts/new/layer4_experts16_moe_freeze_router.sh \
     ${GATE_NAME} ${MoE_INDEX} ${Dynamic_mode} ${Dynamic_steps} ${SAVE_DIR} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
@@ -11,72 +56,28 @@ DATE=0714
 GATE_NAME=CustomNaiveGate
 MoE_INDEX=0,1,2,3
 Dynamic_mode=linear_decrease
-Dynamic_steps=400000
+Dynamic_steps=200000
 SAVE_DIR=Layer4_Experts16_${GATE_NAME}_ALL_${Dynamic_mode}_${Dynamic_steps}
 CUDA_VISIBLE_DEVICES=1 nohup bash scripts/new/layer4_experts16_moe_freeze_router.sh \
     ${GATE_NAME} ${MoE_INDEX} ${Dynamic_mode} ${Dynamic_steps} ${SAVE_DIR} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
 
 DATE=0714
 GATE_NAME=CustomNaiveGate
-MoE_INDEX=0,1,2,3
-Dynamic_mode=cosine_decrease
+MoE_INDEX=1,2,3
+Dynamic_mode=linear_increase
 Dynamic_steps=400000
-SAVE_DIR=Layer4_Experts16_${GATE_NAME}_ALL_${Dynamic_mode}_${Dynamic_steps}
+SAVE_DIR=Layer4_Experts16_${GATE_NAME}_123_${Dynamic_mode}_${Dynamic_steps}
 CUDA_VISIBLE_DEVICES=2 nohup bash scripts/new/layer4_experts16_moe_freeze_router.sh \
     ${GATE_NAME} ${MoE_INDEX} ${Dynamic_mode} ${Dynamic_steps} ${SAVE_DIR} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
 
 DATE=0714
 GATE_NAME=CustomNaiveGate
-MoE_INDEX=0,1,2,3
-Dynamic_mode=cosine_increase
+MoE_INDEX=1,2,3
+Dynamic_mode=linear_decrease
 Dynamic_steps=400000
-SAVE_DIR=Layer4_Experts16_${GATE_NAME}_ALL_${Dynamic_mode}_${Dynamic_steps}
+SAVE_DIR=Layer4_Experts16_${GATE_NAME}_123_${Dynamic_mode}_${Dynamic_steps}
 CUDA_VISIBLE_DEVICES=3 nohup bash scripts/new/layer4_experts16_moe_freeze_router.sh \
     ${GATE_NAME} ${MoE_INDEX} ${Dynamic_mode} ${Dynamic_steps} ${SAVE_DIR} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
-
-
-
-
-
-
-
-
-
-# DATE=0714
-# GATE_NAME=CustomNaiveGate
-# MoE_INDEX=0,1,2,3
-# Dynamic_mode=linear_increase
-# Dynamic_steps=200000
-# SAVE_DIR=Layer4_Experts16_${GATE_NAME}_ALL_${Dynamic_mode}_${Dynamic_steps}
-# CUDA_VISIBLE_DEVICES=0 nohup bash scripts/new/layer4_experts16_moe_freeze_router.sh \
-#     ${GATE_NAME} ${MoE_INDEX} ${Dynamic_mode} ${Dynamic_steps} ${SAVE_DIR} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
-
-# DATE=0714
-# GATE_NAME=CustomNaiveGate
-# MoE_INDEX=0,1,2,3
-# Dynamic_mode=linear_decrease
-# Dynamic_steps=200000
-# SAVE_DIR=Layer4_Experts16_${GATE_NAME}_ALL_${Dynamic_mode}_${Dynamic_steps}
-# CUDA_VISIBLE_DEVICES=0 nohup bash scripts/new/layer4_experts16_moe_freeze_router.sh \
-#     ${GATE_NAME} ${MoE_INDEX} ${Dynamic_mode} ${Dynamic_steps} ${SAVE_DIR} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
-
-# DATE=0714
-# GATE_NAME=CustomNaiveGate
-# MoE_INDEX=1,2,3
-# Dynamic_mode=linear_increase
-# Dynamic_steps=400000
-# SAVE_DIR=Layer4_Experts16_${GATE_NAME}_123_${Dynamic_mode}_${Dynamic_steps}
-# CUDA_VISIBLE_DEVICES=0 nohup bash scripts/new/layer4_experts16_moe_freeze_router.sh \
-#     ${GATE_NAME} ${MoE_INDEX} ${Dynamic_mode} ${Dynamic_steps} ${SAVE_DIR} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
-
-# DATE=0714
-# GATE_NAME=CustomNaiveGate
-# MoE_INDEX=1,2,3
-# Dynamic_mode=linear_decrease
-# Dynamic_steps=400000
-# SAVE_DIR=Layer4_Experts16_${GATE_NAME}_123_${Dynamic_mode}_${Dynamic_steps}
-# CUDA_VISIBLE_DEVICES=0 nohup bash scripts/new/layer4_experts16_moe_freeze_router.sh \
-#     ${GATE_NAME} ${MoE_INDEX} ${Dynamic_mode} ${Dynamic_steps} ${SAVE_DIR} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
 
 
 
