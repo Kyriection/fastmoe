08b34e6950064864ac4750d521571461486d9acf
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-07-19 16:50:33 +0800

    Update val_curve_plot.py

--- examples/transformer-xl/val_curve_plot.py
+++ examples/transformer-xl/val_curve_plot.py
@@ -13,4 +13,14 @@ for line in data:
         eval_losses.append(float(line[-8:-1]))
 
 print(eval_losses)
+x = list(np.array(1, len(eval_losses)+1)* 4000)
+
+
+plt.plot(x, eval_losses)
+plt.ylabel('Eval: bpc')
+plt.xlabel('Iterations')
+plt.title(sys.argv[1])
+plt.savefig('{}.png'.format(sys.argv[1][4:-5]), bbox_inches='tight')
+plt.close()
+
 
