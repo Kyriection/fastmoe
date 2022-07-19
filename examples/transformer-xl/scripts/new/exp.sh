67ce8c2ca0f40a64cef4d7eb43452f367ba15f96
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-07-20 01:35:26 +0800

    1

--- examples/transformer-xl/scripts/new/exp.sh
+++ examples/transformer-xl/scripts/new/exp.sh
@@ -17,7 +17,6 @@ SAVE_DIR=Layer4_Experts16_${GATE_NAME}_ALL_${Dynamic_mode}_${Dynamic_steps}
 CUDA_VISIBLE_DEVICES=1 nohup bash scripts/new/layer4_experts16_moe_freeze_router.sh \
     ${GATE_NAME} ${MoE_INDEX} ${Dynamic_mode} ${Dynamic_steps} ${SAVE_DIR} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
 
-
 DATE=0720
 GATE_NAME=CustomNaiveGate
 MoE_INDEX=0,1,2,3
@@ -27,7 +26,6 @@ SAVE_DIR=Layer4_Experts16_${GATE_NAME}_ALL_${Dynamic_mode}_${Dynamic_steps}_STAB
 CUDA_VISIBLE_DEVICES=2 nohup bash scripts/new/layer4_experts16_moe_router_stable.sh \
     ${GATE_NAME} ${MoE_INDEX} ${Dynamic_mode} ${Dynamic_steps} ${SAVE_DIR} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
 
-
 DATE=0720
 GATE_NAME=CustomNaiveGate
 MoE_INDEX=0,1,2,3
@@ -36,4 +34,44 @@ Dynamic_steps=400000
 SAVE_DIR=Layer4_Experts16_${GATE_NAME}_ALL_${Dynamic_mode}_${Dynamic_steps}_SWAD
 CUDA_VISIBLE_DEVICES=3 nohup bash scripts/new/layer4_experts16_moe_freeze_router_swa.sh \
     ${GATE_NAME} ${MoE_INDEX} ${Dynamic_mode} ${Dynamic_steps} ${SAVE_DIR} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
+
+
+## VITA3 UT ##
+DATE=0720
+GATE_NAME=CustomRandomGate
+MoE_INDEX=0,1,2,3
+Dynamic_mode=cosine_decrease
+Dynamic_steps=400000
+SAVE_DIR=Layer4_Experts16_${GATE_NAME}_ALL_${Dynamic_mode}_${Dynamic_steps}
+CUDA_VISIBLE_DEVICES=0 nohup bash scripts/new/layer4_experts16_moe.sh \
+    ${GATE_NAME} ${MoE_INDEX} ${Dynamic_mode} ${Dynamic_steps} ${SAVE_DIR} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
+
+DATE=0720
+GATE_NAME=CustomRandomGate
+MoE_INDEX=0,1,2,3
+SAVE_DIR=Layer4_Experts16_${GATE_NAME}_ALL
+CUDA_VISIBLE_DEVICES=1 nohup bash scripts/new/layer4_experts16_moe_basic.sh \
+    ${GATE_NAME} ${MoE_INDEX} ${SAVE_DIR} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
+
+
+DATE=0720
+GATE_NAME=CustomRandomGate
+MoE_INDEX=0,1,2,3
+Dynamic_mode=cosine_decrease
+Dynamic_steps=400000
+SAVE_DIR=Layer4_Experts16_${GATE_NAME}_ALL_${Dynamic_mode}_${Dynamic_steps}_SWAD
+CUDA_VISIBLE_DEVICES=2 nohup bash scripts/new/layer4_experts16_moe_swa.sh \
+    ${GATE_NAME} ${MoE_INDEX} ${Dynamic_mode} ${Dynamic_steps} ${SAVE_DIR} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
+
+DATE=0720
+GATE_NAME=CustomRandomGate
+MoE_INDEX=0,1,2,3
+SAVE_DIR=Layer4_Experts16_${GATE_NAME}_ALL_SWAD
+CUDA_VISIBLE_DEVICES=3 nohup bash scripts/new/layer4_experts16_moe_basic_swa.sh \
+    ${GATE_NAME} ${MoE_INDEX} ${SAVE_DIR} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
+
+
+
+
+
 
---
+++ examples/transformer-xl/scripts/new/layer4_experts16_moe_basic_swa.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo 'Run training...'
+python -u train.py \
+    --cuda \
+    --data ../data/enwik8/ \
+    --dataset enwik8 \
+    --n_layer 4 \
+    --d_model 256 \
+    --n_head 8 \
+    --d_head 64 \
+    --d_inner 512 \
+    --dropout 0.1 \
+    --dropatt 0.0 \
+    --optim adam \
+    --lr 0.00025 \
+    --warmup_step 0 \
+    --max_step 400000 \
+    --tgt_len 512 \
+    --mem_len 512 \
+    --eval_tgt_len 128 \
+    --batch_size 22 \
+    --moe --moe-num-expert 16 --moe-top-k 2 \
+    --gate_name $1 \
+    --moe_index $2 \
+    --work_dir $3 \
+    --swad \
+    --swad_start 250000 \
+    --swad_end 400000
