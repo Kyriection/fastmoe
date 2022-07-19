c87382af3203ea12c9d1140e9b9261a4f75fc319
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-07-19 16:27:51 +0800

    Update eval.sh

--- examples/transformer-xl/scripts/new/eval.sh
+++ examples/transformer-xl/scripts/new/eval.sh
@@ -5,7 +5,7 @@ MoE_INDEX=0,1,2,3
 Dynamic_mode=linear_increase
 Dynamic_steps=400000
 SAVE_DIR=Layer4_Experts16_${GATE_NAME}_ALL_${Dynamic_mode}_${Dynamic_steps}
-CUDA_VISIBLE_DEVICES=0 nohup bash scripts/new/layer4_eval.sh \
+CUDA_VISIBLE_DEVICES=2 nohup bash scripts/new/layer4_eval.sh \
     ${GATE_NAME} ${MoE_INDEX} ${Dynamic_mode} ${Dynamic_steps} ${SAVE_DIR} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
 
 DATE=EVAL_0714
@@ -14,7 +14,7 @@ MoE_INDEX=0,1,2,3
 Dynamic_mode=linear_decrease
 Dynamic_steps=400000
 SAVE_DIR=Layer4_Experts16_${GATE_NAME}_ALL_${Dynamic_mode}_${Dynamic_steps}
-CUDA_VISIBLE_DEVICES=1 nohup bash scripts/new/layer4_eval.sh \
+CUDA_VISIBLE_DEVICES=3 nohup bash scripts/new/layer4_eval.sh \
     ${GATE_NAME} ${MoE_INDEX} ${Dynamic_mode} ${Dynamic_steps} ${SAVE_DIR} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
 
 DATE=EVAL_0714
