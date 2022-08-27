ccd31e76794fffa62e87e273fff85ee3c1618c34
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-08-27 01:12:52 -0500

    z

---
+++ examples/transformer-xl/scripts/exp0827/launch.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+if [[ $1 == 'exp1' ]]; then
+    echo 'Run Experiment 1'
+    DATE=0827
+    GATE_NAME=CustomNaiveGate
+    Dynamic_mode=linear_increase
+    NUM_LAYER=4
+    NUM_EXPERT=2
+    MIN_K=1
+    MAX_K=2
+    SAVE_DIR=MoE${NUM_LAYER}-${NUM_EXPERT}_RandomWeight_ALL_${Dynamic_mode}_${MIN_K}_${MAX_K}
+    nohup bash scripts/exp0827/moe_freeze_router.sh \
+        ${GATE_NAME} ${Dynamic_mode} ${MIN_K} ${MAX_K} ${SAVE_DIR} ${NUM_LAYER} ${NUM_EXPERT} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
+
+elif [[ $1 == 'exp2' ]]; then
+    echo 'Run Experiment 2'
+    DATE=0827
+    GATE_NAME=CustomNaiveGate
+    Dynamic_mode=linear_increase
+    NUM_LAYER=4
+    NUM_EXPERT=4
+    MIN_K=2
+    MAX_K=4
+    SAVE_DIR=MoE${NUM_LAYER}-${NUM_EXPERT}_RandomWeight_ALL_${Dynamic_mode}_${MIN_K}_${MAX_K}
+    nohup bash scripts/exp0827/moe_freeze_router.sh \
+        ${GATE_NAME} ${Dynamic_mode} ${MIN_K} ${MAX_K} ${SAVE_DIR} ${NUM_LAYER} ${NUM_EXPERT} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
+
+elif [[ $1 == 'exp3' ]]; then
+    echo 'Run Experiment 3'
+    DATE=0827
+    GATE_NAME=CustomNaiveGate
+    Dynamic_mode=linear_increase
+    NUM_LAYER=4
+    NUM_EXPERT=8
+    MIN_K=4
+    MAX_K=8
+    SAVE_DIR=MoE${NUM_LAYER}-${NUM_EXPERT}_RandomWeight_ALL_${Dynamic_mode}_${MIN_K}_${MAX_K}
+    nohup bash scripts/exp0827/moe_freeze_router.sh \
+        ${GATE_NAME} ${Dynamic_mode} ${MIN_K} ${MAX_K} ${SAVE_DIR} ${NUM_LAYER} ${NUM_EXPERT} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
+
+elif [[ $1 == 'exp4' ]]; then
+    echo 'Run Experiment 4'
+    DATE=0827
+    GATE_NAME=CustomNaiveGate
+    Dynamic_mode=linear_increase
+    NUM_LAYER=4
+    NUM_EXPERT=32
+    MIN_K=16
+    MAX_K=32
+    SAVE_DIR=MoE${NUM_LAYER}-${NUM_EXPERT}_RandomWeight_ALL_${Dynamic_mode}_${MIN_K}_${MAX_K}
+    nohup bash scripts/exp0827/moe_freeze_router.sh \
+        ${GATE_NAME} ${Dynamic_mode} ${MIN_K} ${MAX_K} ${SAVE_DIR} ${NUM_LAYER} ${NUM_EXPERT} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
+
+elif [[ $1 == 'exp5' ]]; then
+    echo 'Run Experiment 5'
+    DATE=0827
+    GATE_NAME=CustomNaiveGate
+    Dynamic_mode=linear_increase
+    NUM_LAYER=4
+    NUM_EXPERT=64
+    MIN_K=32
+    MAX_K=64
+    SAVE_DIR=MoE${NUM_LAYER}-${NUM_EXPERT}_RandomWeight_ALL_${Dynamic_mode}_${MIN_K}_${MAX_K}
+    nohup bash scripts/exp0827/moe_freeze_router.sh \
+        ${GATE_NAME} ${Dynamic_mode} ${MIN_K} ${MAX_K} ${SAVE_DIR} ${NUM_LAYER} ${NUM_EXPERT} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
+
+elif [[ $1 == 'exp6' ]]; then
+    echo 'Run Experiment 6'
+    DATE=0827
+    GATE_NAME=CustomNaiveGate
+    Dynamic_mode=linear_increase
+    NUM_LAYER=2
+    NUM_EXPERT=16
+    MIN_K=8
+    MAX_K=16
+    SAVE_DIR=MoE${NUM_LAYER}-${NUM_EXPERT}_RandomWeight_ALL_${Dynamic_mode}_${MIN_K}_${MAX_K}
+    nohup bash scripts/exp0827/moe_freeze_router.sh \
+        ${GATE_NAME} ${Dynamic_mode} ${MIN_K} ${MAX_K} ${SAVE_DIR} ${NUM_LAYER} ${NUM_EXPERT} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
+
+elif [[ $1 == 'exp7' ]]; then
+    echo 'Run Experiment 7'
+    DATE=0827
+    GATE_NAME=CustomNaiveGate
+    Dynamic_mode=linear_increase
+    NUM_LAYER=8
+    NUM_EXPERT=16
+    MIN_K=8
+    MAX_K=16
+    SAVE_DIR=MoE${NUM_LAYER}-${NUM_EXPERT}_RandomWeight_ALL_${Dynamic_mode}_${MIN_K}_${MAX_K}
+    nohup bash scripts/exp0827/moe_freeze_router.sh \
+        ${GATE_NAME} ${Dynamic_mode} ${MIN_K} ${MAX_K} ${SAVE_DIR} ${NUM_LAYER} ${NUM_EXPERT} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
+
+elif [[ $1 == 'exp8' ]]; then
+    echo 'Run Experiment 8'
+    DATE=0827
+    GATE_NAME=CustomNaiveGate
+    Dynamic_mode=linear_increase
+    NUM_LAYER=12
+    NUM_EXPERT=16
+    MIN_K=8
+    MAX_K=16
+    SAVE_DIR=MoE${NUM_LAYER}-${NUM_EXPERT}_RandomWeight_ALL_${Dynamic_mode}_${MIN_K}_${MAX_K}
+    nohup bash scripts/exp0827/moe_freeze_router.sh \
+        ${GATE_NAME} ${Dynamic_mode} ${MIN_K} ${MAX_K} ${SAVE_DIR} ${NUM_LAYER} ${NUM_EXPERT} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
+
+elif [[ $1 == 'exp9' ]]; then
+    echo 'Run Experiment 9'
+    DATE=0827
+    GATE_NAME=CustomNaiveGate
+    Dynamic_mode=linear_increase
+    NUM_LAYER=4
+    NUM_EXPERT=16
+    MIN_K=8
+    MAX_K=16
+    SAVE_DIR=MoE${NUM_LAYER}-${NUM_EXPERT}_Naive_ALL_${Dynamic_mode}_${MIN_K}_${MAX_K}
+    nohup bash scripts/exp0827/moe.sh \
+        ${GATE_NAME} ${Dynamic_mode} ${MIN_K} ${MAX_K} ${SAVE_DIR} ${NUM_LAYER} ${NUM_EXPERT} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
+
+else
+    echo 'unknown argment 1'
+fi
