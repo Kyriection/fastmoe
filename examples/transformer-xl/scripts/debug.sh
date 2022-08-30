610e05dd375549d528fd5e57af8b82751f9fe145
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-08-29 19:02:07 -0500

    Update debug.sh

--- examples/transformer-xl/scripts/debug.sh
+++ examples/transformer-xl/scripts/debug.sh
@@ -2,8 +2,8 @@
 echo 'Run training...'
 python -u train_csqa.py \
     --cuda \
-    --data ../data/enwik8/ \
-    --dataset enwik8 \
+    --data ../data/csqa/ \
+    --dataset csqa \
     --n_layer 4 \
     --d_model 256 \
     --n_head 8 \
