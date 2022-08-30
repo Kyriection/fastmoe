d8477a029f88e1234f535d93d7981332c20d5c5d
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-08-29 19:52:29 -0500

    Update mem_transformer_csqa.py

--- examples/transformer-xl/mem_transformer_csqa.py
+++ examples/transformer-xl/mem_transformer_csqa.py
@@ -744,7 +744,7 @@ class MemTransformerLM(nn.Module):
 
     def _create_params(self):
 
-        self.project_weight = nn.Parameter(torch.Tensor(self.d_model, 1))
+        self.project_weight = nn.Parameter(torch.Tensor(1, self.d_model))
         self.project_bias = nn.Parameter(torch.Tensor(1))
 
         if self.attn_type == 0: # default attention
