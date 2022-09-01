c71d5281d7d2ea8a633867f2ba84374c83bf4fe5
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-09-01 02:20:10 -0500

    Update mem_transformer_csqa.py

--- examples/transformer-xl/mem_transformer_csqa.py
+++ examples/transformer-xl/mem_transformer_csqa.py
@@ -836,7 +836,7 @@ class MemTransformerLM(nn.Module):
             assert False
         else:
             dec_attn_mask = torch.triu(
-                word_emb.new_ones(qlen, klen), diagonal=1+mlen).byte()[:,:,bsz]
+                word_emb.new_ones(qlen, klen), diagonal=1+mlen).byte()[:,:,None].repeat(1,1,bsz)
             print(dec_attn_mask.shape)
             pdb.set_trace()
             dec_attn_mask = (dec_attn_mask + attn_mask).byte()
