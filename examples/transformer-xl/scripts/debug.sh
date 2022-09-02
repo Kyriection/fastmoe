851090e7063d920f280a597fa1f805a69105101c
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-09-02 16:25:03 -0500

    Update debug.sh

--- examples/transformer-xl/scripts/debug.sh
+++ examples/transformer-xl/scripts/debug.sh
@@ -1,9 +1,41 @@
+# #!/bin/bash
+# echo 'Run training...'
+# python -u train_csqa.py \
+#     --cuda \
+#     --data ../data/csqa/ \
+#     --dataset csqa \
+#     --n_layer 4 \
+#     --d_model 256 \
+#     --n_head 8 \
+#     --d_head 64 \
+#     --d_inner 512 \
+#     --dropout 0.1 \
+#     --dropatt 0.0 \
+#     --optim adam \
+#     --lr $2 \
+#     --warmup_step 0 \
+#     --max_step 4000 \
+#     --eval-interval 500 \
+#     --log-interval 20 \
+#     --tgt_len 512 \
+#     --mem_len 512 \
+#     --eval_tgt_len 128 \
+#     --batch_size 16 \
+#     --moe --moe-num-expert 16 --moe-top-k 2 \
+#     --gate_name CustomNaiveGate \
+#     --moe_index 0,1,2,3 \
+#     --work_dir debug \
+#     --pretrained_weight $1
+
+
+
+
 #!/bin/bash
 echo 'Run training...'
-python -u train_csqa.py \
+python -u train.py \
     --cuda \
-    --data ../data/csqa/ \
-    --dataset csqa \
+    --data ../data/enwik8/ \
+    --dataset enwik8 \
     --n_layer 4 \
     --d_model 256 \
     --n_head 8 \
@@ -12,7 +44,7 @@ python -u train_csqa.py \
     --dropout 0.1 \
     --dropatt 0.0 \
     --optim adam \
-    --lr $2 \
+    --lr 0.00025 \
     --warmup_step 0 \
     --max_step 4000 \
     --eval-interval 500 \
@@ -24,6 +56,5 @@ python -u train_csqa.py \
     --moe --moe-num-expert 16 --moe-top-k 2 \
     --gate_name CustomNaiveGate \
     --moe_index 0,1,2,3 \
-    --work_dir debug \
-    --pretrained_weight $1
+    --work_dir debug 
 
