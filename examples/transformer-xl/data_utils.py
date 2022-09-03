1444908ae82ee033239950db693b0f791e4e24f4
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-09-03 00:04:38 -0500

    Update data_utils.py

--- examples/transformer-xl/data_utils.py
+++ examples/transformer-xl/data_utils.py
@@ -102,6 +102,7 @@ class CSQAIterator(object):
         sublabels = []
 
         for idx in index_list:
+            pdb.set_trace()
             subencoded_0.append(self.encoded_0[idx])
             subencoded_1.append(self.encoded_1[idx])
             subencoded_2.append(self.encoded_2[idx])
