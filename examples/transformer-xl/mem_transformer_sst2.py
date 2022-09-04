a061915e58bc8dc32373f7f339d98013df69eece
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-09-04 16:02:11 -0500

    Update mem_transformer_sst2.py

--- examples/transformer-xl/mem_transformer_sst2.py
+++ examples/transformer-xl/mem_transformer_sst2.py
@@ -858,7 +858,7 @@ class MemTransformerLM(nn.Module):
 
             dec_attn_mask = torch.triu(
                 word_emb.new_ones(qlen, klen), diagonal=1+mlen).byte()[:,:,None].repeat(1,1,bsz)
-            dec_attn_mask = (dec_attn_mask + attn_mask).byte()
+            dec_attn_mask = ((dec_attn_mask + attn_mask) > 0).byte()
         pdb.set_trace()
         hids = []
         if self.attn_type == 0: # default
