0f646640ae061a934b4e1fb4553a2284f45e558e
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-09-03 22:02:07 -0500

    Update vocabulary.py

--- examples/transformer-xl/utils/vocabulary.py
+++ examples/transformer-xl/utils/vocabulary.py
@@ -212,7 +212,7 @@ class Vocab(object):
                 sentence, label = line[0], int(line[1])
                 assert label in [0,1]
                 sentence_toks = self.tokenize(sentence, add_eos=add_eos, add_double_eos=add_double_eos, add_cls_token=add_cls_token)
-                encoded.append(sentence_toks)
+                encoded.append(self.convert_to_tensor(sentence_toks))
                 labels.append(label)
 
         labels = torch.LongTensor(labels)
