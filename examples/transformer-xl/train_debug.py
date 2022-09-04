38f87ac4cfadbe3cfc2b4cb3b377dc3427709a3a
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-09-03 23:16:04 -0500

    Update train_debug.py

--- examples/transformer-xl/train_debug.py
+++ examples/transformer-xl/train_debug.py
@@ -351,6 +351,7 @@ args.n_nonemb_param = sum([p.nelement() for p in model.layers.parameters()])
 from bert import Transformer, Config
 cfg = Config()
 cfg.from_json('a.json')
+print(ntokens)
 cfg.vocab_size = ntokens
 print(ntokens)
 
