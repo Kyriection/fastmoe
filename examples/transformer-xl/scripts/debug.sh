396467270b32c7c2bc2e65bdb5608cab1c09de45
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-09-03 22:34:53 -0500

    Update debug.sh

--- examples/transformer-xl/scripts/debug.sh
+++ examples/transformer-xl/scripts/debug.sh
@@ -71,7 +71,7 @@ python -u train_sst2.py \
     --dropout 0.1 \
     --dropatt 0.0 \
     --optim adam \
-    --lr 0.00025 \
+    --lr $2 \
     --warmup_step 0 \
     --max_step 4000 \
     --eval-interval 500 \
