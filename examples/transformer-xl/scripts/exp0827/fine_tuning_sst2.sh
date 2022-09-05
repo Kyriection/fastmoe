193a269372d5942046dfb080318ac2bd689814bf
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-09-05 16:29:17 -0500

    Create fine_tuning_sst2.sh

---
+++ examples/transformer-xl/scripts/exp0827/fine_tuning_sst2.sh
@@ -0,0 +1,27 @@
+echo 'Run training...'
+python -u train_sst2.py \
+    --cuda \
+    --data ../glue_data/SST-2 \
+    --dataset sst2 \
+    --n_layer 4 \
+    --d_model 256 \
+    --n_head 8 \
+    --d_head 64 \
+    --d_inner 512 \
+    --dropout 0.1 \
+    --dropatt 0.0 \
+    --optim adam \
+    --lr $2 \
+    --warmup_step 0 \
+    --max_step 5000 \
+    --eval-interval 500 \
+    --log-interval 100 \
+    --tgt_len 512 \
+    --mem_len 512 \
+    --eval_tgt_len 128 \
+    --batch_size 16 \
+    --work_dir debug \
+    --pretrained_weight $1 \
+    --moe --moe-num-expert 16 --moe-top-k 2 \
+    --gate_name CustomNaiveGate \
+    --freeze_gate 
