7db6d2ebc6e8d0e29dab2027ebf08d75055d6a46
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-09-05 15:28:05 -0500

    Update mem_transformer_sst2_v2.py

--- examples/transformer-xl/mem_transformer_sst2_v2.py
+++ examples/transformer-xl/mem_transformer_sst2_v2.py
@@ -809,7 +809,7 @@ class MemTransformerLM(nn.Module):
             end_idx = mlen + max(0, qlen - 0 - self.ext_len)
             beg_idx = max(0, end_idx - self.mem_len)
             for i in range(len(hids)):
-
+                print(hids[i].shape, attn_mask.shape)
                 cat = torch.cat([mems[i], hids[i]], dim=0)
                 new_mems.append(cat[beg_idx:end_idx].detach())
                 print(cat.shape)
