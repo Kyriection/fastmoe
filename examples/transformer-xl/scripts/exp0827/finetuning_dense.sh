38b9e6c3eac25fe1a96e7d55906cf2aece873e23
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-09-05 20:31:57 -0500

    1

---
+++ examples/transformer-xl/scripts/exp0827/finetuning_dense.sh
@@ -0,0 +1,24 @@
+echo 'Run training...'
+python -u train_sst2.py \
+    --cuda \
+    --data ../glue_data/SST-2 \
+    --dataset sst2 \
+    --n_layer 4 \
+    --d_model 256 \
+    --n_head 8 \
+    --d_head 64 \
+    --d_inner $3 \
+    --dropout 0.1 \
+    --dropatt 0.0 \
+    --optim adam \
+    --lr $2 \
+    --warmup_step 0 \
+    --max_step 5000 \
+    --eval-interval 500 \
+    --log-interval 100 \
+    --tgt_len 512 \
+    --mem_len 512 \
+    --eval_tgt_len 128 \
+    --batch_size 16 \
+    --work_dir dense_$3_finetune \
+    --pretrained_weight $1 
--- examples/transformer-xl/scripts/exp0827/run_finetune.sh
+++ examples/transformer-xl/scripts/exp0827/run_finetune.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0 nohup bash scripts/exp0827/fine_tuning_sst2.sh $1 1e-4 $2 > log_dense_$2_lr1e-4_sst2.out 2>&1 &
-CUDA_VISIBLE_DEVICES=1 nohup bash scripts/exp0827/fine_tuning_sst2.sh $1 1e-5 $2 > log_dense_$2_lr1e-5_sst2.out 2>&1 &
-CUDA_VISIBLE_DEVICES=2 nohup bash scripts/exp0827/fine_tuning_sst2.sh $1 5e-5 $2 > log_dense_$2_lr5e-5_sst2.out 2>&1 &
-CUDA_VISIBLE_DEVICES=3 nohup bash scripts/exp0827/fine_tuning_sst2.sh $1 5e-4 $2 > log_dense_$2_lr5e-4_sst2.out 2>&1 &
+CUDA_VISIBLE_DEVICES=0 nohup bash scripts/exp0827/finetuning_dense.sh $1 1e-4 $2 > log_dense_$2_lr1e-4_sst2.out 2>&1 &
+CUDA_VISIBLE_DEVICES=1 nohup bash scripts/exp0827/finetuning_dense.sh $1 1e-5 $2 > log_dense_$2_lr1e-5_sst2.out 2>&1 &
+CUDA_VISIBLE_DEVICES=2 nohup bash scripts/exp0827/finetuning_dense.sh $1 5e-5 $2 > log_dense_$2_lr5e-5_sst2.out 2>&1 &
+CUDA_VISIBLE_DEVICES=3 nohup bash scripts/exp0827/finetuning_dense.sh $1 5e-4 $2 > log_dense_$2_lr5e-4_sst2.out 2>&1 &
