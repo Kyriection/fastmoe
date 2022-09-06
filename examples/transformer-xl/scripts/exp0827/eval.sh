3310098813d793db3e6de3aa10b5510dd520a81b
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-09-05 23:01:28 -0500

    Update eval.sh

--- examples/transformer-xl/scripts/exp0827/eval.sh
+++ examples/transformer-xl/scripts/exp0827/eval.sh
@@ -1,6 +1,33 @@
-#!/bin/bash
+# #!/bin/bash
+# echo 'Run Evaluation...'
+# CUDA_VISIBLE_DEVICES=$3 nohup python -u evaluation.py \
+#     --cuda \
+#     --data ../data/enwik8/ \
+#     --dataset enwik8 \
+#     --n_layer 4 \
+#     --d_model 256 \
+#     --n_head 8 \
+#     --d_head 64 \
+#     --d_inner $1 \
+#     --dropout 0.1 \
+#     --dropatt 0.0 \
+#     --optim adam \
+#     --lr 0.00025 \
+#     --warmup_step 0 \
+#     --max_step 400000 \
+#     --tgt_len 512 \
+#     --mem_len 512 \
+#     --eval_tgt_len 128 \
+#     --batch_size 22 \
+#     --checkpoint_weight $2 > log_eval_dense_dim$1.out 2>&1 &
+
+
+
+
+
+
 echo 'Run Evaluation...'
-CUDA_VISIBLE_DEVICES=$3 nohup python -u evaluation.py \
+CUDA_VISIBLE_DEVICES=$2 nohup python -u evaluation.py \
     --cuda \
     --data ../data/enwik8/ \
     --dataset enwik8 \
@@ -8,7 +35,7 @@ CUDA_VISIBLE_DEVICES=$3 nohup python -u evaluation.py \
     --d_model 256 \
     --n_head 8 \
     --d_head 64 \
-    --d_inner $1 \
+    --d_inner 512 \
     --dropout 0.1 \
     --dropatt 0.0 \
     --optim adam \
@@ -19,4 +46,6 @@ CUDA_VISIBLE_DEVICES=$3 nohup python -u evaluation.py \
     --mem_len 512 \
     --eval_tgt_len 128 \
     --batch_size 22 \
-    --checkpoint_weight $2 > log_eval_dense_dim$1.out 2>&1 &
+    --moe --moe-num-expert 16 --moe-top-k 2 \
+    --gate_name CustomNaiveGate \
+    --checkpoint_weight $1 > log_eval_dense_dim$3.out 2>&1 &
