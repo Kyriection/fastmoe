e0adfe6da10177f05cfcc5eeeab173490ad22157
Allen <39230030+Kyriection@users.noreply.github.com>
2022-09-20 00:01:11 -0500

    Update ddl.sh

--- examples/transformer-xl/scripts/exp0827/ddl.sh
+++ examples/transformer-xl/scripts/exp0827/ddl.sh
@@ -20,7 +20,7 @@ NUM_EXPERT=16
 MIN_K=4
 MAX_K=16
 SAVE_DIR=MoE${NUM_LAYER}-${NUM_EXPERT}_RandomWeight_ALL_${Dynamic_mode}_${MIN_K}_${MAX_K}
-CUDA_VISIBLE_DEVICES=0,1,2,3 nohup bash scripts/exp0827/moe_freeze_router.sh \
+CUDA_VISIBLE_DEVICES=4,5,6,7 nohup bash scripts/exp0827/moe_freeze_router.sh \
     ${GATE_NAME} ${Dynamic_mode} ${MIN_K} ${MAX_K} ${SAVE_DIR} ${NUM_LAYER} ${NUM_EXPERT} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
 
 
