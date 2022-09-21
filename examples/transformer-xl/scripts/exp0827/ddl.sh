d1238b29acfda766b12a3c5e8da518017a0e1f59
Allen <39230030+Kyriection@users.noreply.github.com>
2022-09-21 16:12:35 -0500

    Update ddl.sh

--- examples/transformer-xl/scripts/exp0827/ddl.sh
+++ examples/transformer-xl/scripts/exp0827/ddl.sh
@@ -66,14 +66,28 @@
 
 
 
-echo 'Run SMOE TOP-8'
+# echo 'Run SMOE TOP-8'
+# DATE=0919
+# GATE_NAME=CustomNaiveGate
+# Dynamic_mode=linear_increase
+# NUM_LAYER=4
+# NUM_EXPERT=16
+# MIN_K=8
+# MAX_K=8
+# SAVE_DIR=MoE${NUM_LAYER}-${NUM_EXPERT}_Naive_ALL_${Dynamic_mode}_${MIN_K}_${MAX_K}
+# CUDA_VISIBLE_DEVICES=2 nohup bash scripts/exp0827/moe_single.sh \
+#     ${GATE_NAME} ${Dynamic_mode} ${MIN_K} ${MAX_K} ${SAVE_DIR} ${NUM_LAYER} ${NUM_EXPERT} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
+
+
+
+echo 'Run SMOE TOP-10'
 DATE=0919
 GATE_NAME=CustomNaiveGate
 Dynamic_mode=linear_increase
 NUM_LAYER=4
 NUM_EXPERT=16
-MIN_K=8
-MAX_K=8
+MIN_K=10
+MAX_K=10
 SAVE_DIR=MoE${NUM_LAYER}-${NUM_EXPERT}_Naive_ALL_${Dynamic_mode}_${MIN_K}_${MAX_K}
-CUDA_VISIBLE_DEVICES=2 nohup bash scripts/exp0827/moe_single.sh \
+CUDA_VISIBLE_DEVICES=3 nohup bash scripts/exp0827/moe_single.sh \
     ${GATE_NAME} ${Dynamic_mode} ${MIN_K} ${MAX_K} ${SAVE_DIR} ${NUM_LAYER} ${NUM_EXPERT} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
