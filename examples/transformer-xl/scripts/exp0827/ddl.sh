f7326113e5e7e42cb389115aa764a6eb6e9d9f8e
Allen <39230030+Kyriection@users.noreply.github.com>
2022-09-21 16:32:41 -0500

    Update ddl.sh

--- examples/transformer-xl/scripts/exp0827/ddl.sh
+++ examples/transformer-xl/scripts/exp0827/ddl.sh
@@ -25,30 +25,30 @@
 
 
 
-echo 'Run RMT TOP-12'
-DATE=0921
-GATE_NAME=CustomNaiveGate
-Dynamic_mode=linear_increase
-NUM_LAYER=4
-NUM_EXPERT=16
-MIN_K=12
-MAX_K=12
-SAVE_DIR=MoE${NUM_LAYER}-${NUM_EXPERT}_RandomWeight_ALL_${Dynamic_mode}_${MIN_K}_${MAX_K}
-CUDA_VISIBLE_DEVICES=4,5,6,7 nohup bash scripts/exp0827/moe_freeze_router.sh \
-    ${GATE_NAME} ${Dynamic_mode} ${MIN_K} ${MAX_K} ${SAVE_DIR} ${NUM_LAYER} ${NUM_EXPERT} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
-
-
-# echo 'Run SMOE TOP-4'
-# DATE=0919
+# echo 'Run RMT TOP-12'
+# DATE=0921
 # GATE_NAME=CustomNaiveGate
 # Dynamic_mode=linear_increase
 # NUM_LAYER=4
 # NUM_EXPERT=16
-# MIN_K=4
-# MAX_K=4
-# SAVE_DIR=MoE${NUM_LAYER}-${NUM_EXPERT}_Naive_ALL_${Dynamic_mode}_${MIN_K}_${MAX_K}
-# CUDA_VISIBLE_DEVICES=0 nohup bash scripts/exp0827/moe_single.sh \
+# MIN_K=12
+# MAX_K=12
+# SAVE_DIR=MoE${NUM_LAYER}-${NUM_EXPERT}_RandomWeight_ALL_${Dynamic_mode}_${MIN_K}_${MAX_K}
+# CUDA_VISIBLE_DEVICES=4,5,6,7 nohup bash scripts/exp0827/moe_freeze_router.sh \
 #     ${GATE_NAME} ${Dynamic_mode} ${MIN_K} ${MAX_K} ${SAVE_DIR} ${NUM_LAYER} ${NUM_EXPERT} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
+
+
+echo 'Run SMOE TOP-4'
+DATE=0919
+GATE_NAME=CustomNaiveGate
+Dynamic_mode=linear_increase
+NUM_LAYER=4
+NUM_EXPERT=16
+MIN_K=4
+MAX_K=4
+SAVE_DIR=MoE${NUM_LAYER}-${NUM_EXPERT}_Naive_ALL_${Dynamic_mode}_${MIN_K}_${MAX_K}
+nohup bash scripts/exp0827/moe.sh \
+    ${GATE_NAME} ${Dynamic_mode} ${MIN_K} ${MAX_K} ${SAVE_DIR} ${NUM_LAYER} ${NUM_EXPERT} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
 
 
 
