230801f18ffd56d6c19b338f595abbf5d86307ba
Allen <39230030+Kyriection@users.noreply.github.com>
2022-09-23 02:11:45 -0500

    1

--- examples/transformer-xl/scripts/exp0827/ddl.sh
+++ examples/transformer-xl/scripts/exp0827/ddl.sh
@@ -25,17 +25,17 @@
 
 
 
-# echo 'Run RMT TOP-12'
-# DATE=0921
-# GATE_NAME=CustomNaiveGate
-# Dynamic_mode=linear_increase
-# NUM_LAYER=4
-# NUM_EXPERT=16
-# MIN_K=12
-# MAX_K=12
-# SAVE_DIR=MoE${NUM_LAYER}-${NUM_EXPERT}_RandomWeight_ALL_${Dynamic_mode}_${MIN_K}_${MAX_K}
-# CUDA_VISIBLE_DEVICES=4,5,6,7 nohup bash scripts/exp0827/moe_freeze_router.sh \
-#     ${GATE_NAME} ${Dynamic_mode} ${MIN_K} ${MAX_K} ${SAVE_DIR} ${NUM_LAYER} ${NUM_EXPERT} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
+echo 'Run RMT TOP-12'
+DATE=0921
+GATE_NAME=CustomNaiveGate
+Dynamic_mode=linear_increase
+NUM_LAYER=4
+NUM_EXPERT=16
+MIN_K=12
+MAX_K=12
+SAVE_DIR=MoE${NUM_LAYER}-${NUM_EXPERT}_RandomWeight_ALL_${Dynamic_mode}_${MIN_K}_${MAX_K}
+CUDA_VISIBLE_DEVICES=4,5,6,7 nohup bash scripts/exp0827/moe_freeze_router.sh \
+    ${GATE_NAME} ${Dynamic_mode} ${MIN_K} ${MAX_K} ${SAVE_DIR} ${NUM_LAYER} ${NUM_EXPERT} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
 
 
 # echo 'Run SMOE TOP-4'
@@ -78,14 +78,14 @@
 #     ${GATE_NAME} ${Dynamic_mode} ${MIN_K} ${MAX_K} ${SAVE_DIR} ${NUM_LAYER} ${NUM_EXPERT} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
 
 
-echo 'Run SMOE TOP-10'
-DATE=0919_Test
-GATE_NAME=CustomNaiveGate
-Dynamic_mode=linear_increase
-NUM_LAYER=4
-NUM_EXPERT=16
-MIN_K=10
-MAX_K=10
-SAVE_DIR=MoE${NUM_LAYER}-${NUM_EXPERT}_Naive_ALL_${Dynamic_mode}_${MIN_K}_${MAX_K}
-nohup bash scripts/exp0827/moe.sh \
-    ${GATE_NAME} ${Dynamic_mode} ${MIN_K} ${MAX_K} ${SAVE_DIR} ${NUM_LAYER} ${NUM_EXPERT} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
+# echo 'Run SMOE TOP-10'
+# DATE=0919_Test
+# GATE_NAME=CustomNaiveGate
+# Dynamic_mode=linear_increase
+# NUM_LAYER=4
+# NUM_EXPERT=16
+# MIN_K=10
+# MAX_K=10
+# SAVE_DIR=MoE${NUM_LAYER}-${NUM_EXPERT}_Naive_ALL_${Dynamic_mode}_${MIN_K}_${MAX_K}
+# nohup bash scripts/exp0827/moe.sh \
+#     ${GATE_NAME} ${Dynamic_mode} ${MIN_K} ${MAX_K} ${SAVE_DIR} ${NUM_LAYER} ${NUM_EXPERT} > log_${DATE}_${SAVE_DIR}.out 2>&1 &
