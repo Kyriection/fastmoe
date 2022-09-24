cfd2fc3dfb7af68c3c38385352a7c42657aa6650
Allen <39230030+Kyriection@users.noreply.github.com>
2022-09-23 22:22:43 -0500

    Update eval.sh

--- examples/transformer-xl/scripts/exp0827/eval.sh
+++ examples/transformer-xl/scripts/exp0827/eval.sh
@@ -26,32 +26,6 @@
 
 
 
-# echo 'Run Evaluation...'
-# CUDA_VISIBLE_DEVICES=$2 nohup python -u evaluation.py \
-#     --cuda \
-#     --data ../data/enwik8/ \
-#     --dataset enwik8 \
-#     --n_layer 4 \
-#     --d_model 256 \
-#     --n_head 8 \
-#     --d_head 64 \
-#     --d_inner 512 \
-#     --dropout 0.1 \
-#     --dropatt 0.0 \
-#     --optim adam \
-#     --lr 0.00025 \
-#     --warmup_step 0 \
-#     --max_step 400000 \
-#     --tgt_len 512 \
-#     --mem_len 512 \
-#     --eval_tgt_len 128 \
-#     --batch_size 22 \
-#     --moe --moe-num-expert 16 --moe-top-k 2 \
-#     --gate_name CustomNaiveGate \
-#     --checkpoint_weight $1 > log_eval_$3.out 2>&1 &
-
-
-
 echo 'Run Evaluation...'
 CUDA_VISIBLE_DEVICES=$2 nohup python -u evaluation.py \
     --cuda \
@@ -73,5 +47,31 @@ CUDA_VISIBLE_DEVICES=$2 nohup python -u evaluation.py \
     --eval_tgt_len 128 \
     --batch_size 22 \
     --moe --moe-num-expert 16 --moe-top-k 2 \
-    --gate_name CustomRandomGate \
+    --gate_name CustomNaiveGate \
     --checkpoint_weight $1 > log_eval_$3.out 2>&1 &
+
+
+
+# echo 'Run Evaluation...'
+# CUDA_VISIBLE_DEVICES=$2 nohup python -u evaluation.py \
+#     --cuda \
+#     --data ../data/enwik8/ \
+#     --dataset enwik8 \
+#     --n_layer 4 \
+#     --d_model 256 \
+#     --n_head 8 \
+#     --d_head 64 \
+#     --d_inner 512 \
+#     --dropout 0.1 \
+#     --dropatt 0.0 \
+#     --optim adam \
+#     --lr 0.00025 \
+#     --warmup_step 0 \
+#     --max_step 400000 \
+#     --tgt_len 512 \
+#     --mem_len 512 \
+#     --eval_tgt_len 128 \
+#     --batch_size 22 \
+#     --moe --moe-num-expert 16 --moe-top-k 2 \
+#     --gate_name CustomRandomGate \
+#     --checkpoint_weight $1 > log_eval_$3.out 2>&1 &
