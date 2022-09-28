875c99e7100660079d83e480ef0651c94b7dc11b
Allen <39230030+Kyriection@users.noreply.github.com>
2022-09-28 02:21:16 -0500

    Create concrete_plan_b.sh

---
+++ examples/transformer-xl/scripts/exp0827/concrete_plan_b.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+CUDA_VISIBLE_DEVICES=1 nohup python -u train_concrete.py \
+    --cuda \
+    --data ../data/enwik8/ \
+    --dataset enwik8 \
+    --n_layer 4 \
+    --d_model 256 \
+    --n_head 8 \
+    --d_head 64 \
+    --d_inner 8192 \
+    --dropout 0.1 \
+    --dropatt 0.0 \
+    --optim adam \
+    --lr 0.00025 \
+    --warmup_step 0 \
+    --max_step 40000 \
+    --tgt_len 512 \
+    --mem_len 512 \
+    --eval_tgt_len 128 \
+    --batch_size 44 \
+    --work_dir big_dense_concrete > log_0928_big_dense_concrete_4w.out 2>&1 &
