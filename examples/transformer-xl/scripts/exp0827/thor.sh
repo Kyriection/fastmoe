ac72a675ba5f0a6521f45d4c2e0df4cd2f15494e
Allen <39230030+Kyriection@users.noreply.github.com>
2022-09-27 21:26:37 -0500

    Update thor.sh

--- examples/transformer-xl/scripts/exp0827/thor.sh
+++ examples/transformer-xl/scripts/exp0827/thor.sh
@@ -14,16 +14,14 @@ python -u train_thor.py \
     --optim adam \
     --lr 0.00025 \
     --warmup_step 0 \
-    --max_step 34375 \
+    --max_step 100000
     --tgt_len 512 \
     --mem_len 512 \
     --eval_tgt_len 128 \
-    --batch_size 256 \
+    --batch_size 88 \
     --multi_gpu \
     --gpu0_bsz 32 \
     --moe --moe-num-expert 16 --moe-top-k 2 \
     --gate_name CustomRandomGate \
     --work_dir THOR_4layer_16experts \
-    --kl_alpha 2 \
-    --restart \
-    --restart_dir $1
+    --kl_alpha 2 
