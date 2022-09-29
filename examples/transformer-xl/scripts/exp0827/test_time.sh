6e1d31406fa2456d9dd7acd6edcd702d593771c5
Allen <39230030+Kyriection@users.noreply.github.com>
2022-09-29 04:36:17 -0500

    Update test_time.sh

--- examples/transformer-xl/scripts/exp0827/test_time.sh
+++ examples/transformer-xl/scripts/exp0827/test_time.sh
@@ -43,7 +43,32 @@
 #     --work_dir big_dense 
 
 
-python -u train_thor.py \
+# python -u train_thor.py \
+#     --cuda \
+#     --data ../data/enwik8/ \
+#     --dataset enwik8 \
+#     --n_layer 4 \
+#     --d_model 256 \
+#     --n_head 8 \
+#     --d_head 64 \
+#     --d_inner 512 \
+#     --dropout 0.1 \
+#     --dropatt 0.0 \
+#     --optim adam \
+#     --lr 0.00025 \
+#     --warmup_step 0 \
+#     --max_step 100000 \
+#     --tgt_len 512 \
+#     --mem_len 512 \
+#     --eval_tgt_len 128 \
+#     --batch_size 22 \
+#     --moe --moe-num-expert 16 --moe-top-k 2 \
+#     --gate_name CustomRandomGate \
+#     --work_dir THOR_4layer_16experts \
+#     --kl_alpha 2 
+
+
+python -u train.py \
     --cuda \
     --data ../data/enwik8/ \
     --dataset enwik8 \
@@ -57,12 +82,12 @@ python -u train_thor.py \
     --optim adam \
     --lr 0.00025 \
     --warmup_step 0 \
-    --max_step 100000 \
+    --max_step 200000 \
     --tgt_len 512 \
     --mem_len 512 \
     --eval_tgt_len 128 \
     --batch_size 22 \
-    --moe --moe-num-expert 16 --moe-top-k 2 \
-    --gate_name CustomRandomGate \
-    --work_dir THOR_4layer_16experts \
-    --kl_alpha 2 
+    --moe --moe-num-expert 16 --moe-top-k $1 \
+    --gate_name CustomNaiveGate \
+    --work_dir test_dir \
+    --log-interval 50 
