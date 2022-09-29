a8e326e00bfed9513014d9a46463e46007151a17
Allen <39230030+Kyriection@users.noreply.github.com>
2022-09-29 04:31:50 -0500

    Update test_time.sh

--- examples/transformer-xl/scripts/exp0827/test_time.sh
+++ examples/transformer-xl/scripts/exp0827/test_time.sh
@@ -21,7 +21,29 @@
 #     --work_dir big_dense_block 
 
 
-python -u train.py \
+# python -u train.py \
+#     --cuda \
+#     --data ../data/enwik8/ \
+#     --dataset enwik8 \
+#     --n_layer 4 \
+#     --d_model 256 \
+#     --n_head 8 \
+#     --d_head 64 \
+#     --d_inner 8192 \
+#     --dropout 0.1 \
+#     --dropatt 0.0 \
+#     --optim adam \
+#     --lr 0.00025 \
+#     --warmup_step 0 \
+#     --max_step 400000 \
+#     --tgt_len 512 \
+#     --mem_len 512 \
+#     --eval_tgt_len 128 \
+#     --batch_size 22 \
+#     --work_dir big_dense 
+
+
+python -u train_thor.py \
     --cuda \
     --data ../data/enwik8/ \
     --dataset enwik8 \
@@ -29,15 +51,18 @@ python -u train.py \
     --d_model 256 \
     --n_head 8 \
     --d_head 64 \
-    --d_inner 8192 \
+    --d_inner 512 \
     --dropout 0.1 \
     --dropatt 0.0 \
     --optim adam \
     --lr 0.00025 \
     --warmup_step 0 \
-    --max_step 400000 \
+    --max_step 100000 \
     --tgt_len 512 \
     --mem_len 512 \
     --eval_tgt_len 128 \
     --batch_size 22 \
-    --work_dir big_dense 
+    --moe --moe-num-expert 16 --moe-top-k 2 \
+    --gate_name CustomRandomGate \
+    --work_dir THOR_4layer_16experts \
+    --kl_alpha 2 
