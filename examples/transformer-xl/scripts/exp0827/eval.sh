18d6ea1a6ebaa3c3a26e2e31293264b62a056ade
Allen <39230030+Kyriection@users.noreply.github.com>
2022-09-29 02:08:36 -0500

    Update eval.sh

--- examples/transformer-xl/scripts/exp0827/eval.sh
+++ examples/transformer-xl/scripts/exp0827/eval.sh
@@ -24,32 +24,7 @@
 
 
 
-CUDA_VISIBLE_DEVICES=5 python -u evaluation.py \
-    --cuda \
-    --data ../data/enwik8/ \
-    --dataset enwik8 \
-    --n_layer 4 \
-    --d_model 256 \
-    --n_head 8 \
-    --d_head 64 \
-    --d_inner 512 \
-    --dropout 0.1 \
-    --dropatt 0.0 \
-    --optim adam \
-    --lr 0.00025 \
-    --warmup_step 0 \
-    --max_step 400000 \
-    --tgt_len 512 \
-    --mem_len 512 \
-    --eval_tgt_len 128 \
-    --batch_size 22 \
-    --moe --moe-num-expert 16 --moe-top-k 2 \
-    --gate_name CustomHashGate \
-    --checkpoint_weight $1 
-
-
-# echo 'Run Evaluation...'
-# CUDA_VISIBLE_DEVICES=$2 nohup python -u evaluation.py \
+# CUDA_VISIBLE_DEVICES=5 python -u evaluation.py \
 #     --cuda \
 #     --data ../data/enwik8/ \
 #     --dataset enwik8 \
@@ -70,7 +45,32 @@ CUDA_VISIBLE_DEVICES=5 python -u evaluation.py \
 #     --batch_size 22 \
 #     --moe --moe-num-expert 16 --moe-top-k 2 \
 #     --gate_name CustomHashGate \
-#     --checkpoint_weight $1 > log_eval_$3.out 2>&1 &
+#     --checkpoint_weight $1 
+
+
+echo 'Run Evaluation...'
+CUDA_VISIBLE_DEVICES=$2 nohup python -u evaluation.py \
+    --cuda \
+    --data ../data/enwik8/ \
+    --dataset enwik8 \
+    --n_layer 4 \
+    --d_model 256 \
+    --n_head 8 \
+    --d_head 64 \
+    --d_inner 512 \
+    --dropout 0.1 \
+    --dropatt 0.0 \
+    --optim adam \
+    --lr 0.00025 \
+    --warmup_step 0 \
+    --max_step 400000 \
+    --tgt_len 512 \
+    --mem_len 512 \
+    --eval_tgt_len 128 \
+    --batch_size 22 \
+    --moe --moe-num-expert 16 --moe-top-k 2 \
+    --gate_name CustomHashGate \
+    --checkpoint_weight $1 > log_eval_$3.out 2>&1 &
 
 
 
