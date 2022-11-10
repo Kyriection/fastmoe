00a561e7a73d22cf6ce92b188cceba210cbca447
Allen <39230030+Kyriection@users.noreply.github.com>
2022-11-10 06:25:20 -0600

    1

---
+++ examples/transformer-xl/scripts/rebuttal/launch.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=0 nohup python -u script/rebuttal/test_ex2.sh > log_rebuttal_test_Ex2.out &
+CUDA_VISIBLE_DEVICES=1 nohup python -u script/rebuttal/test_ex4.sh > log_rebuttal_test_Ex4.out &
+CUDA_VISIBLE_DEVICES=2 nohup python -u script/rebuttal/test_ex8.sh > log_rebuttal_test_Ex8.out &
+CUDA_VISIBLE_DEVICES=3 nohup python -u script/rebuttal/test_ex16.sh > log_rebuttal_test_Ex16.out &
---
+++ examples/transformer-xl/scripts/rebuttal/test_ex2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+echo 'Run training...'
+python -u train.py \
+    --cuda \
+    --data ../data/enwik8/ \
+    --dataset enwik8 \
+    --n_layer 4 \
+    --d_model 256 \
+    --n_head 8 \
+    --d_head 64 \
+    --d_inner 4096 \
+    --dropout 0.1 \
+    --dropatt 0.0 \
+    --optim adam \
+    --lr 0.00025 \
+    --warmup_step 0 \
+    --max_step 20000 \
+    --tgt_len 512 \
+    --mem_len 512 \
+    --eval_tgt_len 128 \
+    --batch_size 88 \
+    --moe --moe-num-expert 2 --moe-top-k 2 \
+    --gate_name CustomNaiveGate \
+    --freeze_gate \
+    --dynamic_moe \
+    --dynamic_moe_mode linear_increase \
+    --dynamic_overall_steps 20000 \
+    --moe-top-k-min 1 \
+    --moe-top-k-max 2 \
+    --work_dir EXPERTS2_Test
