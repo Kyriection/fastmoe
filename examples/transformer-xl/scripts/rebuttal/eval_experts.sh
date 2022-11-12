c72dd96b0a73e80c98f5a094194759a57f7d7ad7
Allen <39230030+Kyriection@users.noreply.github.com>
2022-11-11 23:55:48 -0600

    Create eval_experts.sh

---
+++ examples/transformer-xl/scripts/rebuttal/eval_experts.sh
@@ -0,0 +1,23 @@
+echo 'Run Evaluation...'
+CUDA_VISIBLE_DEVICES=$3 nohup python -u evaluation.py \
+    --cuda \
+    --data ../data/enwik8/ \
+    --dataset enwik8 \
+    --n_layer 4 \
+    --d_model 256 \
+    --n_head 8 \
+    --d_head 64 \
+    --d_inner 512 \
+    --dropout 0.1 \
+    --dropatt 0.0 \
+    --optim adam \
+    --lr 0.00025 \
+    --warmup_step 0 \
+    --max_step 400000 \
+    --tgt_len 512 \
+    --mem_len 512 \
+    --eval_tgt_len 128 \
+    --batch_size 22 \
+    --moe --moe-num-expert $2 --moe-top-k 2 \
+    --gate_name CustomNaiveGate \
+    --checkpoint_weight $1 > log_eval_exp$2.out 2>&1 &
